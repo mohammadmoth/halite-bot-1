78b7532d46715d7590322d6616040c39c263357c
mohmmad.m.othman <mohmmad.m.othman@gmail.com>
2020-06-30 12:58:29 +0200

    Updating

--- MyBot.py
+++ MyBot.py
@@ -21,6 +21,11 @@ while True:
     قرار احتلال العدو او الكواكب الفارغة يعتمد على عدد الكواكب المحتلة
     في حال كان احتل نسبة تصل 40% من لعبة يقوم بتجميع القوات والتنقل من كوكب الى كوكب للاحتال السريع
 
+
+    تطويرات اضافية 
+    البحث عن سفن العدو 
+    سفني المصابة تنسحب الى مكان امن للمعالجة في كوكب ما
+    اقلاع السفن من الكوكب و الهجوم
     """
     alltroops = Map.get_me().all_ships()
     listofplantdock = []
@@ -29,15 +34,14 @@ while True:
         targetplanet = []
         targetarrmy = []
 
-        if arrmy.docking_status != arrmy.DockingStatus.UNDOCKED:
-
+        if arrmy.docking_status == arrmy.DockingStatus.DOCKING and arrmy.docking_status == arrmy.DockingStatus.UNDOCKING:
             continue
-
 
         entities_by_distance_all = Map.nearby_entities_by_distance(arrmy)
 
-        entities_by_distance_all = sorted(
-            entities_by_distance_all.items(), key=lambda entities_by_distance_allX: entities_by_distance_allX[0], reverse=False)
+       
+        entities_by_distance_all = sorted(entities_by_distance_all.items(), key=lambda entities_by_distance_allX: entities_by_distance_allX[0], reverse=False)
+
 
         for target in entities_by_distance_all:
             if isinstance(target[1][0], hlt.entity.Planet) and (not target[1][0].is_owned() or AttackOwnerPlanet):
@@ -49,17 +53,17 @@ while True:
        # logging.info(targetplanet)
        # logging.info(targetarrmy)
 
-        if len(targetplanet) == 0 and turn >100:
+        if len(targetplanet) == 0:
             AttackOwnerPlanet = True
             ignore_planttacksame = True
-        
+
         for planet in targetplanet:
             if arrmy.can_dock(planet) and (not planet.is_owned() or planet.owner.id != Map.my_id) and not planet in listofplantdock:
                 if AttackOwnerPlanet:
                     listofplantdock.append(planet)
-                
+
                 commands.append(arrmy.dock(planet))
-                
+
                 break
             else:
                 if planet in Planet_has_been_attacked and not ignore_planttacksame:
@@ -68,19 +72,19 @@ while True:
                 else:
                     if not planet.is_owned() or planet.owner.id != Map.my_id:
                         xy = arrmy.closest_point_to(planet)
-                        if AttackOwnerPlanet : 
+                        if AttackOwnerPlanet and turn > 100:
                             xy = planet.closest_point_to(arrmy)
                         navigate_command = arrmy.navigate(
                             xy,
                             Map,
-                            speed=int(hlt.constants.MAX_SPEED),
+                            speed=int(7),
                             ignore_ships=False)
                     else:
                         continue
                     if navigate_command:
                         commands.append(navigate_command)
                         Planet_has_been_attacked.append(planet)
-                        
+
                         break
     game.send_command_queue(commands)
     # next
